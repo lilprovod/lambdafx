1d1b2a7e2dd897831b8bb9ed27d71b1dd04d9ff1
drsharapov_24 <daniilsharapow@yandex.ru>
2026-05-10 20:53:11 +0300

    style: change integer literals to double

--- src/equation.c
+++ src/equation.c
@@ -15,10 +15,10 @@ double equation(double lambda, double a)
 {
     if (lambda < 1.0) return NAN;
 
-    double term1 = sqrt(lambda * (lambda - 1));
+    double term1 = sqrt(lambda * (lambda - 1.0));
     
     double root1 = sqrt(lambda);
-    double root2 = sqrt(lambda - 1);
+    double root2 = sqrt(lambda - 1.0);
     double term2 = log(root1 + root2);
 
     double function = term1 + term2 - a;
