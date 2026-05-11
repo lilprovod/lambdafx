702c31571b3f26f1714661e59fff1a1f6f840cf8
drsharapov_24 <daniilsharapow@yandex.ru>
2026-05-11 17:00:19 +0300

    refactor: remove extra else block

--- src/io.c
+++ src/io.c
@@ -89,29 +89,29 @@ bool parse_args(int argc, char* argv[], InputData* data)
     if (argc < 2 || argc > 3) {
         print_usage();
         return false;
-    } else {
-        char* endptr;
-        data->parameter_a = strtod(argv[1], &endptr);
+    }
 
-        if (argv[1] == endptr || *endptr != '\0') {
-            fprintf(stderr, "Incorrect value for parameter a: %s\n", argv[1]);
-            return false;
-        }
-        if (data->parameter_a < 0.0) {
-            fprintf(stderr, "Right part of equation must be positive: %s\n", argv[1]);
-            return false;
-        }
+    char* endptr;
+    data->parameter_a = strtod(argv[1], &endptr);
 
-        if (argc == 3) {
-            if (strcmp(argv[2], "--debug") == 0) {
-                data->debug_enabled = true;
-            } else {
-                fprintf(stderr, "Unknown argument: %s\n", argv[2]);
-                return false;
-            }
+    if (argv[1] == endptr || *endptr != '\0') {
+        fprintf(stderr, "Incorrect value for parameter a: %s\n", argv[1]);
+        return false;
+    }
+    if (data->parameter_a < 0.0) {
+        fprintf(stderr, "Right part of equation must be positive: %s\n", argv[1]);
+        return false;
+    }
+
+    if (argc == 3) {
+        if (strcmp(argv[2], "--debug") == 0) {
+            data->debug_enabled = true;
         } else {
-            data->debug_enabled = false;
+            fprintf(stderr, "Unknown argument: %s\n", argv[2]);
+            return false;
         }
+    } else {
+        data->debug_enabled = false;
     }
 
     return true;
