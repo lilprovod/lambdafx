616c995a54f08f808f2cfe5e5688984651bbd3f9
drsharapov_24 <daniilsharapow@yandex.ru>
2026-05-11 17:03:06 +0300

    style: change magic number to DBL_DIG

--- src/comparison.c
+++ src/comparison.c
@@ -1,3 +1,4 @@
+#include <float.h>
 #include <stddef.h>
 #include <math.h>
 
@@ -13,7 +14,7 @@ double delta(double value1, double value2)
 int matching_digits(double value1, double value2)
 {
     double diff = delta(value1, value2);
-    if (diff == 0.0) return 15;
+    if (diff == 0.0) return DBL_DIG;
 
     int digits = (int)-floor(log10(diff));
 
